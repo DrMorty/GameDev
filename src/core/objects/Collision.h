d54adc47680b23b0a0058f214f08f21c88c8d4bd
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-04-18 17:48:39 +0300

    Create Collision.h

---
+++ src/core/objects/Collision.h
@@ -0,0 +1,43 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+#include "GameObjectComponent.h"
+
+#include "SFML/Graphics.hpp"
+
+#include <iostream>
+
+namespace engine
+{
+  class Collision;
+  
+  struct CollisionDetails
+  {
+    CollisionDetails(Collision* object1, Collision* object2, float depth, sf::Vector2f trajectory)
+    Collider1 (object1), Collider2(object2), depth(depth), collisionTrajectory(trajectory)
+    {};
+    
+    Collision* Collider1 = 0;
+    Collision* Collider2 = 0;
+    
+    float depth;
+    sf::Vector2f collisionTrajectory;
+  };
+  
+  class Collision: public GameObjectComponent
+  {
+    public:
+      Collision();
+      ~Collision() {};
+      
+      void setCollision(float x1, float y1, float x2, float y2);
+      void setCollisionWithSprite();
+      
+     sf::Vector2f leftTop;
+     sf::Vector2f rightDown;
+     
+     bool isDynamic = true;
+  };
+}
+
+#endif
