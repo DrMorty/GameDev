1b18e3e01c49982144d3c0095b14d2685cc44087
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-04-18 16:37:32 +0300

    Update input.cpp

--- src/core/Control/input.cpp
+++ src/core/Control/input.cpp
@@ -21,5 +21,49 @@ namespace engine
         return false;
     }
     
+    bool Input::isKeyUp(KeyCode key)
+    {
+        auto keyPressedEvents = Engine::instance()->logicsManager->eventManager.getAllEventsOfType(sf::Event::KeyReleased);
+
+        for (auto& event : keyPressedEvents)
+            if (event.key.code == key)
+                return true;
+
+        return false;
+    }
+
+    void Input::keyPressed(sf::Keyboard::Key key)
+    {
+        auto keyCode = convertFromSfmlKey(key);
+
+        isKeyPressed[keyCode] = true;
+
+        if (isKeyHold[keyCode])
+            isKeyPressed[keyCode] = false;
+
+        isKeyHold[keyCode] = true;
+    }
     
     
+        void Input::keyReleased(sf::Keyboard::Key key)
+    {
+        auto keyCode = convertFromSfmlKey(key);
+
+        isKeyPressed[keyCode] = false;
+        isKeyHold[keyCode] = false;
+    }
+    
+     KeyCode Input::convertFromSfmlKey(sf::Keyboard::Key key)
+    {
+        switch(key)
+        {
+            case KeyCode::W:        return W;
+            case KeyCode::A:        return A;
+            case KeyCode::S:        return S;
+            case KeyCode::D:        return D;
+            case KeyCode::E:        return E;
+            case KeyCode::Q:        return Q;
+            case KeyCode::Space:    return Space;
+        }
+    }
+}
