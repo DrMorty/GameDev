ffe6c11de0236859a0056d075104ccc3bc785eb1
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-04-18 17:18:16 +0300

    Create LogicManager.h

---
+++ src/core/management/LogicManager.h
@@ -0,0 +1,32 @@
+#ifndef LOGIC_MANAGER_H
+#define LOGIC_MANAGER_H
+
+#include <vector>
+#include <list>
+#include <map>
+
+#include "../storage/GameObject.h"
+#include "EventManager.h"
+
+namespace engine
+{   
+    class LogicManager
+    {
+    public:
+        void updateLogic();
+        void callStartForScripts();
+        void clearNotStarted()
+        void callUpdateForScripts();
+        void callOnCollisionForObject(GameObject* object, CollisionDetails& details);
+
+        void registerScript(Script* script);
+        void unregisterScript(Script* script);
+
+        EventManager eventManager;
+    private:
+        std::list<Script*> notStartedScripts;
+        std::vector<Script*> scripts; 
+    };
+}
+
+#endif 
