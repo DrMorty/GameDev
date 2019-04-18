159ddae0543d17f6f47f4b30476e378e1b4d8356
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-04-18 16:42:27 +0300

    Create Camera.h

---
+++ src/core/Control/Camera.h
@@ -0,0 +1,20 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "../common/math/Math.h"
+
+namespace engine
+{
+    class Camera
+    {
+     public:
+        static Vector3 projectionToCamera(Vector3 objectPosition);
+
+     private:
+        Camera();
+
+        static Vector3 position;
+    };
+}
+
+#endif
