ae21e843aa90573c65851820df5d129b285e3771
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-04-18 17:56:59 +0300

    Update renderer.cpp

--- src/core/objects/renderer.cpp
+++ src/core/objects/renderer.cpp
@@ -1,6 +1,32 @@
-#include "renderer.h"
+#include <iostream>
 
-Renderer::Renderer()
-{
+#include "Renderer.h"
+#include "../Engine.h"
 
+namespace engine
+{   
+    void Renderer2D::setSprite(const sf::Texture& t_texture)
+    {
+        texture = t_texture;
+        sprite.setTexture(texture);
+    }
+
+    void Renderer2D::setTexture(const sf::Texture& t_texture)
+    {
+        setSprite(t_texture);
+    }
+
+    void Renderer2D::draw(sf::RenderWindow& windowInstance)
+    {
+        Vector3 textureSize = texture.getSize();
+        Vector3 newPosition = Camera::projectionToCamera(object->transform.position - Vector3(textureSize.x, -textureSize.y) / 2);
+
+        sprite.setPosition(newPosition);
+        Engine::instance()->renderManager->renderObject(sprite);
+    }
+
+    sf::Texture& Renderer2D::getTexture()
+    {
+        return texture;
+    }
 }
