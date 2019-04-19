311348f6bcf451adbc7c1beaba9e87950c070395
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-04-19 15:18:52 +0300

    Update and rename engine.h to Engine.h

---
+++ src/core/Engine.h
@@ -0,0 +1,54 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include <memory>
+
+#include "management/RenderManager.h"
+#include "management/LogicsManager.h"
+#include "management/PhysicsManager.h"
+#include "storage/DataStorage.h"
+
+#include "controllers/Input.h"
+#include "controllers/Time.h"
+#include "controllers/Camera.h"
+
+using engine::Renderer2D;
+using engine::BoxCollider;
+using engine::CollisionDetails;
+using engine::Script;
+
+namespace engine
+{
+    class Engine
+    {
+    public: 
+
+        static Engine* instance();
+        ~Engine();
+
+        void run();
+
+        std::unique_ptr<RenderManager> renderManager;
+        std::unique_ptr<LogicsManager> logicsManager;
+        std::unique_ptr<DataStorage> dataStorage;
+
+    private:
+        Engine();
+
+        static Engine* m_instance;
+    };
+}
+
+namespace engineX
+{
+    using namespace engine;
+
+    void createObject(std::string name);
+    GameObject& getObject(std::string name);
+    void deleteObject(std::string name);
+    void deleteObject(GameObject* object);
+
+    void runScene();
+}
+
+#endif
