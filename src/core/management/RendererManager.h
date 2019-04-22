954e93647e7d4b43749bf414b2ee49f5dbd7cfc1
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-04-22 17:25:29 +0300

    Create RendererManager.h

---
+++ src/core/management/RendererManager.h
@@ -0,0 +1,31 @@
+#ifndef RENDER_MANAGER_H
+#define RENDER_MANAGER_H
+
+#include <SFML/Graphics.hpp>
+#include "../object_components/Renderer2D.h"
+#include "../object_components/BoxCollider.h"
+
+namespace engine
+{   
+    class RenderManager
+    {
+    public:
+        RenderManager();
+
+        void renderDrawableObjects();
+
+        void registerRenderer(Renderer* renderer);
+        void unregisterRenderer(Renderer* renderer);
+
+        void registerCollider(BoxCollider* collider);
+        void unregisterCollider(BoxCollider* collider);
+
+    private:
+        std::vector<Renderer*> renderers;
+        std::vector<BoxCollider*> colliders;
+
+        void renderCollider(BoxCollider* collider);
+    };
+}
+
+#endif
