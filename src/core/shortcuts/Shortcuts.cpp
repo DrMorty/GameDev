40fd4e160fad3ad6db1fa184452c2248a3b64a88
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-04-22 19:38:01 +0300

    Create Shortcuts.cpp

---
+++ src/core/shortcuts/Shortcuts.cpp
@@ -0,0 +1,37 @@
+#include "tools.h"
+#include <iostream>
+#include "../engine.h"
+#include <iterator>
+
+namespace engineShorty
+{
+    void createObject(std::string name)
+    {
+        auto& v = Engine::instance()->dataStorage->getGameObjects();
+
+        if (v.find(name) != v.end())
+            throw GameObjectAlreadyExist;
+
+        v.try_emplace(name, name);
+    }
+
+    GameObject& getObject(std::string name)
+    {
+        return Engine::instance()->dataStorage->gameObjects[name];
+    }
+
+    void deleteObject(std::string name)
+    {
+        Engine::instance()->dataStorage->gameObjects.erase(name);
+    }
+
+    void deleteObject(GameObject* object)
+    {
+        Engine::instance()->dataStorage->gameObjects.erase(object->name);
+    }
+
+    void run()
+    {
+        Engine::instance()->engineRun();
+    }
+}
