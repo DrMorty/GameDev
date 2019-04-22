3ed0c5a5b3328344cf74beb06b02a149c9429ffb
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-04-22 20:21:23 +0300

    Create GameControlScript.h

---
+++ src/core/GameControl/GameControlScript.h
@@ -0,0 +1,32 @@
+#include "Time.h"
+#include "KeyboardInput.h"
+#include "Camera.h"
+
+namespace engine
+{
+  class GameControlScript
+    {
+     public:
+        static bool getKey(KeyCode key);
+        static bool isKeyDown(KeyCode key);
+        static bool isKeyUp(KeyCode key);
+
+        static void keyPressed(sf::Keyboard::Key key);
+        static void keyReleased(sf::Keyboard::Key key);
+
+        static std::map<KeyCode, bool> isKeyHold;
+        static std::map<KeyCode, bool> isKeyPressed;
+        
+        static float deltaTime;
+        static void updateTime();
+
+     private:
+        KeyboardInput();
+        static KeyCode convertFromSfmlKey(sf::Keyboard::Key key)
+        Camera();
+
+        static Vector3 position;  
+   };
+}
+
+#endif
