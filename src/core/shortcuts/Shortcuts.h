68fccc9b5b793bfb856c5eed3eb99e9362ec6e7a
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-04-22 19:34:27 +0300

    Create Shortcuts.h

---
+++ src/core/shortcuts/Shortcuts.h
@@ -0,0 +1,20 @@
+#ifndef SHORTCUTS_H
+#define SHORTCUTS_H
+#include "../objects/gameobject.h"
+#include <string>
+#include "core/engine.h"
+#include "scripts/picturescript.h"
+
+
+
+namespace engineShorty
+{
+    using namespace engine;
+    void createObject(std::string name);
+    void deleteObject(std::string name);
+    void deleteObject(GameObject* object);
+    GameObject* findObject(std::string name);
+
+}
+
+#endif
