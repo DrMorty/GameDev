9b57889e70c5dc0ecbbe83e9946db2d288b98ecc
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-04-22 19:27:33 +0300

    Update Engine.cpp

--- src/core/Engine.cpp
+++ src/core/Engine.cpp
@@ -12,14 +12,14 @@ namespace engine
 
     EngineException GameObjectAlreadyExist;
 
-    Engine* Engine::m_instance = 0;
+    Engine* Engine::prev_instance = 0;
 
     Engine::Engine()
     {
-        logicsManager = std::make_unique<LogicsManager>();
-        renderManager = std::make_unique<RenderManager>();
-        physicsManager = std::make_unique<PhysicsManager>();
-        dataStorage = std::make_unique<DataStorage>();
+        logicsManager = new LogicsManager();
+        renderManager = new RenderManager();
+        physicsManager = new PhysicsManager();
+        dataStorage = new DataStorage();
 
         Metrics::enableLogging();
     };
@@ -30,54 +30,19 @@ namespace engine
 
     Engine* Engine::instance()
     {
-        if (!m_instance)
+        if (!prev_instance)
         {
-            m_instance = new Engine();
+            prev_instance = new Engine();
         }
 
-        return m_instance;
+        return prev_instance;
     }
 
     void Engine::run()
     {
-        for(;;)
-        {
-            Metrics::updateMetrics();
-            logicsManager -> updateLogics();
-            renderManager -> renderDrawableObjects();       
-        }
+        Metrics::updateMetrics();
+        logicsManager -> updateLogics();
+        renderManager -> renderDrawableObjects(); 
     };
 }
 
-namespace engineX
-{
-    void createObject(std::string name)
-    {
-        auto& v = Engine::instance()->dataStorage->getGameObjects();
-
-        if (v.find(name) != v.end())
-            throw GameObjectAlreadyExist;
-
-        v.try_emplace(name, name);
-    }
-
-    GameObject& getObject(std::string name)
-    {
-        return Engine::instance()->dataStorage->gameObjects[name];
-    }
-
-    void deleteObject(std::string name)
-    {
-        Engine::instance()->dataStorage->gameObjects.erase(name);
-    }
-
-    void deleteObject(GameObject* object)
-    {
-        Engine::instance()->dataStorage->gameObjects.erase(object->name);
-    }
-
-    void runScene()
-    {
-        Engine::instance()->run();
-    }
-}
