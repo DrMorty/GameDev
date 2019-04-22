ada9475b3407a41a2898e703fff413879a4e709f
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-04-22 18:33:40 +0300

    Update Renderer.cpp

--- src/core/objects/Renderer.cpp
+++ src/core/objects/Renderer.cpp
@@ -7,8 +7,7 @@ namespace engine
 {   
     void Renderer2D::setSprite(const sf::Texture& t_texture)
     {
-        texture = t_texture;
-        sprite.setTexture(texture);
+        sprite.setTexture(t_texture);
     }
 
     void Renderer2D::setTexture(const sf::Texture& t_texture)
@@ -16,7 +15,7 @@ namespace engine
         setSprite(t_texture);
     }
 
-    void Renderer2D::draw(sf::RenderWindow& windowInstance)
+    void Renderer2D::draw(sf::RenderWindow& window)
     {
         Vector3 textureSize = texture.getSize();
         Vector3 newPosition = Camera::projectionToCamera(object->transform.position - Vector3(textureSize.x, -textureSize.y) / 2);
