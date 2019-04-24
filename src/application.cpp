9ab8c0231f0a224b4445ec61951b6704b45dcb02
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-04-24 15:39:17 +0300

    Update application.cpp

--- src/application.cpp
+++ src/application.cpp
@@ -5,11 +5,37 @@
 
 Engine* Engine::ex_instance = 0;
 
-//write your game here
+class Square : public Script
+{
+ public:
+    void start()
+    {
+        object->transform.position.y = -350;
+        object->transform.position.x = 350;
+    }
+
+    void update()
+    {
+        if (engineS::Input::getKey(KeyCode::A))
+            object->transform.position.x -= 0.5;
+
+        if (engineS::Input::getKey(KeyCode::D))
+            object->transform.position.x += 0.5;
+            
+        if (engineS::Input::getKey(KeyCode::S))
+            object->transform.position.y -= 0.5;
+
+        if (engineS::Input::getKey(KeyCode::W))
+            object->transform.position.y += 0.5;
+    }
+};
 
 
 int main()
 {
+    engineS::createObject("square");
+    engineS::getObject("square").addComponent<Square>();
     
-    //start your game here
+    
+    engineS::run();
 }
