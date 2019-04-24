77ea171baf665dd2ae5de5263abc8cb7eaba9406
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-04-24 18:56:50 +0300

    Create RigidBody.h

---
+++ src/core/objects/RigidBody.h
@@ -0,0 +1,28 @@
+#ifndef RIGID_BODY_H
+#define RIGID_BODY_H
+
+#include "GameObjectComponent.h"
+#include "../common/math/Math.h"
+
+namespace engine
+{   
+    class RigidBody : public GameObjectComponent
+    {
+    public:
+        ~RigidBody() 
+        {};
+
+        float gravityValue = 1.0f;
+        bool isGravity = true;
+
+        float mass = 1.0f;
+
+        Vector3 velocity;
+        Vector3 prevPosition;
+
+    private:
+        
+    };
+}
+
+#endif
