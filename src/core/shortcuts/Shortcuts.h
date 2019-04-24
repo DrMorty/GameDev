ed11a24fdac686f4458ac74487aeebe0e193d260
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-04-24 20:53:05 +0300

    Update Shortcuts.h

--- src/core/shortcuts/Shortcuts.h
+++ src/core/shortcuts/Shortcuts.h
@@ -1,9 +1,24 @@
 #ifndef SHORTCUTS_H
 #define SHORTCUTS_H
 #include "../objects/GameObjectComponent.h"
-#include <string>
-#include "../Engine.h"
 
+#include <string>
+#include <memory>
+
+#include "management/RenderManager.h"
+#include "management/LogicManager.h"
+#include "management/PhysicsManager.h"
+#include "storage/DataStorage.h"
+#include "shortcuts/Shortcuts.h"
+
+#include "GameControl/ControlFPS.h"
+#include "GameControl/GameControlScript.h"
+#include "GameControl/Camera.h"
+
+using engine::Renderer;
+using engine::Collision;
+using engine::CollisionDetails;
+using engine::Script;
 
 
 
