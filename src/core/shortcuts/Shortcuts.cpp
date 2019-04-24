0a7ddc35381173a1f0df67a0eee5ce74c4d41202
Tema1801 <39492184+Tema1801@users.noreply.github.com>
2019-04-24 20:46:13 +0300

    Update Shortcuts.cpp

--- src/core/shortcuts/Shortcuts.cpp
+++ src/core/shortcuts/Shortcuts.cpp
@@ -1,6 +1,24 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include <memory>
 #include <iostream>
-#include "../engine.h"
 #include <iterator>
+
+#include "management/RenderManager.h"
+#include "management/LogicManager.h"
+#include "management/PhysicsManager.h"
+#include "storage/DataStorage.h"
+#include "shortcuts/Shortcuts.h"
+
+#include "GameControl/ControlFPS.h"
+#include "GameControl/GameControlScript.h"
+#include "GameControl/Camera.h"
+
+using engine::Renderer;
+using engine::Collision;
+using engine::CollisionDetails;
+using engine::Script;
 
 namespace engineS
 {
