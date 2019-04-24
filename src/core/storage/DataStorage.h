fcc243997ef47a2b183938177d73ee0b7b4c7a66
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-04-24 19:21:34 +0300

    Update DataStorage.h

--- src/core/storage/DataStorage.h
+++ src/core/storage/DataStorage.h
@@ -1,29 +1,43 @@
-#ifndef DATASTORAGE_H
-#define DATASTORAGE_H
-#include <SFML/Graphics.hpp>
-#include <fstream>
-#include "../objects/gameobject.h"
+    
+#ifndef DATA_STORAGE_H
+#define DATA_STORAGE_H
+
+#include <map>
+#include <string>
+#include <memory>
+
+#include "GameObject.h"
 
 namespace engine
-{
-    struct Window
+{   
+    struct WindowSettings
     {
         std::string name;
+
         int width;
         int height;
     };
-    
+
     const Window HD_Window = {"HD", 1280, 720}
-    const Window Full_HD_Window = {"HD", 1920, 1080}
-    
-    class Datastorage
+    const Window Full_HD_Window = {"FHD", 1920, 1080}
+
+
+    class DataStorage
     {
-       
     public:
-        DataStorage(Window window = HD_window);
+
+        DataStorage(WindowSettings windowSettings = HD);
         ~DataStorage();
+
+        sf::RenderWindow& getWindowInstance();
         std::map<std::string, GameObject>& getGameObjects();
 
         std::map<std::string, GameObject> gameObjects;
-        
-#endif 
+
+    private:
+        sf::RenderWindow windowInstance;
+    };
+}
+
+#endif
+
