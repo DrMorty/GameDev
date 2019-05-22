3810d818ec14601b7e099b28af1e51612421a147
DrMorty <31830532+DrMorty@users.noreply.github.com>
2019-05-22 23:54:53 +0300

    Update main.cpp

--- New Engine/main.cpp
+++ New Engine/main.cpp
@@ -10,11 +10,14 @@ int main()
     myAPI.add_components(0,"Renderer");
     myAPI.add_components(0,"RigidBody");
     myAPI.add_components(0,"Script");
+    myAPI.engine.data_storage->game_objects[0].change_coordinates(500,650);
+    myAPI.engine.data_storage->game_objects[0].change_velocity((0,0));
     myAPI.create_game_object();
     myAPI.add_components(1,"Renderer");
     myAPI.add_components(1,"RigidBody");
     myAPI.add_components(1,"Script");
-    myAPI.engine.data_storage->game_objects[0].change_coordinates(400,200);
+    myAPI.engine.data_storage->game_objects[1].change_coordinates(500,0);
+
     myAPI.engine.main_while();
 
 
